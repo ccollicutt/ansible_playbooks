b90f4c180ddac35e10d96ac8910f5ae95115f4b4
curtis <curtis@curtiss-MacBook-Pro.local>
2012-08-02 22:00:28 -0600

    added some more checks to the bootstrap script

--- vcl/README.md
+++ vcl/README.md
@@ -5,7 +5,13 @@ This playbook will install Apache VCL 2.3 on a single CentOS 6 host.
 
 There is a [blog post](http://www.cybera.ca/tech-radar/first-look-ansible) that describes a bit about how to install VCL with this playbook, though it is a bit older now.
 
-Using Vagrant
+RPM
+---
+
+Note that this playbook installs VCL via custom RPMs. The spec file for the RPMs can be found [here](https://github.com/cybera/rpmspecs/blob/master/vcl.spec)
+
+
+Via Vagrant
 -------------
 
 ```shell
@@ -15,11 +21,13 @@ vagrant_host$ git clone ${thisrepo}
 vagrant_host$ vagrant up #box will build...
 vagrant_host$ vagrant ssh
 centos6$ cd /vagrant/ansible_playbooks/vcl
-centos6$ sudo ./bootstrap_ansible.sh
+centos6$ sudo ./bootstrap_ansible.sh #must run as root
 centos6$ cp vars/example-main.yml vars/main.yml
 centos6$ vi vars/main.yml #edit the file
 centos6$ ssh root@localhost #do this once to accept the host key
 root@centos6$ exit
-centos6$ ansible-playbook all.yml # and wait
+centos6$ export ANSIBLE_HOSTS=`pwd`/ansible_hosts #set the env variable for ansible 
+centos6$ ansible -m ping 127.0.0.1 #test connecitivity to root@localhost
+centos6$ ansible-playbook all.yml #and wait...
 ```
 
--- vcl/bootstrap_ansible.sh
+++ vcl/bootstrap_ansible.sh
@@ -1,28 +1,47 @@
 #!/bin/bash
 
-if ! rpm -qa | grep epel-release; then
-    rpm -ivh http://fedora.mirror.nexicom.net/epel/6/i386/epel-release-6-7.noarch.rpm
+NEW_REPO=no
+
+if [[ $UID -ne 0 ]]; then
+    echo "$0 must be run as root"
+    exit 1
 fi
 
-if ! which ansible; then
-    #yum install -y ansible
-    yum localinstall --nogpg http://packages.serverascode.com/mrepo/custom-centos6-noarch/RPMS.all/ansible-0.5-1.el6.noarch.rpm
+if ! rpm -qa | grep epel-release > /dev/null; then
+    rpm -ivh http://fedora.mirror.nexicom.net/epel/6/i386/epel-release-6-7.noarch.rpm
+    NEW_REPO=yes
 fi
+
+if ! rpm -qa | grep rpmforge > /dev/null; then
+	rpm -ivh http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
+	NEW_REPO=yes
+fi
+
+if [ "$NEW_REPO" == "yes" ]; then
+	yum clean all
+	yum makcecache
+fi
+
+if ! rpm -qa ansible > /dev/null ; then
+    yum install -y ansible
+fi
+
+if [ ! -e ./ansible_hosts ]; then
 
 cat << EOANSIBLE_HOSTS > ./ansible_hosts
 [vcl]
 127.0.0.1
 EOANSIBLE_HOSTS
 
+fi
+
 export ANSIBLE_HOSTS=`pwd`/ansible_hosts
 
-mkdir /root/.ssh
-
-chmod 700 /root/.ssh
-/usr/bin/ssh-keygen -q -t dsa -C '' -N '' -f /home/vagrant/.ssh/id_dsa
-chown vagrant:vagrant /home/vagrant/.ssh/id_dsa*
-cat /home/vagrant/.ssh/id_dsa.pub >> /root/.ssh/authorized_keys
-
-chmod 600 /root/.ssh/authorized_keys
-
-ansible -m ping 127.0.0.1
+if [ ! -e /root/.ssh/authorized_keys ]; then
+	mkdir /root/.ssh
+	chmod 700 /root/.ssh
+	/usr/bin/ssh-keygen -q -t dsa -C '' -N '' -f /home/vagrant/.ssh/id_dsa
+	chown vagrant:vagrant /home/vagrant/.ssh/id_dsa*
+	cat /home/vagrant/.ssh/id_dsa.pub >> /root/.ssh/authorized_keys
+	chmod 600 /root/.ssh/authorized_keys
+fi
