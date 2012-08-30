4e22dfd6af3c574cd7a8cc6587a7804a2a8bf6e6
cyb3ra <cyb3ra@server01.server01.cybera.ca>
2012-08-30 09:35:23 -0600

    a few changes for environment, additional files...

--- pxe_boot/files/network-config.sh
+++ pxe_boot/files/network-config.sh
@@ -1,14 +1,10 @@
-echo "# This file describes the network interfaces available on your system
-# and how to activate them. For more information, see interfaces(5).
-
-# The loopback network interface
+echo "# The loopback network interface
 auto lo
 iface lo inet loopback
 
-# The primary network interface
 auto eth0
 iface eth0 inet static
-	address 192.168.255.108
+	address 192.168.255.102
 	netmask 255.255.255.0
 
 auto eth2
@@ -27,7 +23,7 @@ iface bond0 inet manual
 
 auto vlan422
 iface vlan422 inet static
-	address 199.116.233.20
+	address 199.116.233.19
 	netmask 255.255.255.240
 	gateway 199.116.233.17
 	dns-nameservers 8.8.8.8
@@ -35,6 +31,6 @@ iface vlan422 inet static
 
 auto vlan423
 iface vlan423 inet static
-	address 192.168.1.108
+	address 192.168.1.102
 	netmask 255.255.255.0
 	vlan-raw-device bond0" > /etc/network/interfaces
