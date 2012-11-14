d9318337c5bb3d7b64e8d0c0eb0ee8d5d758e647
curtis <curtis@serverascode.com>
2012-11-14 14:54:05 -0700

    added flavor to openstackImageNameMap

--- vcl/files/openstack_api_modifications.sql
+++ vcl/files/openstack_api_modifications.sql
@@ -2,6 +2,6 @@ insert into module (id, name, prettyname, description, perlpackage) values (28, 
 insert into provisioning (id, name, prettyname, moduleid) values (11, 'openstack_nova', 'Openstack Nova', 28);
 insert into OSinstalltype (id, name) values (6, 'openstack_nova');
 insert into provisioningOSinstalltype (provisioningid, OSinstalltypeid) values (11, 6);
-create table openstackImageNameMap(openstackimagename VARCHAR(60), vclimagename VARCHAR(60));
+create table openstackImageNameMap(openstackimagename VARCHAR(60), vclimagename VARCHAR(60)), flavor TINYINT;
 # According to: https://issues.apache.org/jira/browse/VCL-590?focusedCommentId=13416496#comment-13416496 moduleid should be 5 for linux
 insert into OS (id,name,prettyname,type,installtype,sourcepath,moduleid) values (45, "rhel6openstack", "CentOS 6 OpenStack", "linux", "openstack_nova", "centos6", 5);
