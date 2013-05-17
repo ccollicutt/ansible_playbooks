b343d088c391466cb150eab43c210ff30d76b565
curtis <curtis@wwdyn185.cs.ualberta.ca>
2013-05-17 15:49:52 -0600

    adding distributed mysql

--- distributed_mysql/roles/percona/templates/bootstrap.sql
+++ distributed_mysql/roles/percona/templates/bootstrap.sql
@@ -1,4 +1,4 @@
-UPDATE mysql.user SET password=PASSWORD("Passw0rd") where user='root';
-CREATE USER 'sstuser'@'localhost' IDENTIFIED BY 's3cret';
-GRANT RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO 'sstuser'@'%';
+UPDATE mysql.user SET password=PASSWORD("{{ root_mysql_password }}") where user='root';
+CREATE USER '{{ state_snapshot_transfer_user }}'@'localhost' IDENTIFIED BY '{{ state_snapshot_transfer_password }}';
+GRANT RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO '{{ state_snapshot_transfer_user }}'@'%';
 FLUSH PRIVILEGES;
