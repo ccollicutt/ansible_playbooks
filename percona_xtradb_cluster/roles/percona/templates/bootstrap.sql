713348eac36e9dc9f4d321f00c42e314b44eded9
curtis <curtis@cloud01.private.vclcloud.cybera.ca>
2013-05-28 16:53:27 -0600

    removed bootstrap.sql bc it was moved into the tasks

--- percona_xtradb_cluster/roles/percona/templates/bootstrap.sql
+++
@@ -1,4 +0,0 @@
-UPDATE mysql.user SET password=PASSWORD("{{ root_mysql_password }}") where user='root';
-CREATE USER '{{ state_snapshot_transfer_user }}'@'localhost' IDENTIFIED BY '{{ state_snapshot_transfer_password }}';
-GRANT RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO '{{ state_snapshot_transfer_user }}'@'%';
-FLUSH PRIVILEGES;
